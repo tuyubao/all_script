9950b13eab1ccfabccc1b5d20c63fd785225a321
tyb <19840981323@163.com>
2023-02-16 15:29:07 +0800

    add 安全优化

--- shell/optimize.sh
+++ shell/optimize.sh
@@ -2,3 +2,8 @@
 sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+# 安全优化
+systemctl disable firewalld --now
+setenforce 0		
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config  
