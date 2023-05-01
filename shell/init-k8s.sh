968cac8ee1410bd3efd1ec270f789f27fbd38d09
tyb <19840981323@163.com>
2023-05-01 16:25:31 +0800

    add system initialize script

---
+++ shell/init-k8s.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# 关闭交换分区
+swapoff -a 		# 临时关闭
+sed -i 's/^[^#].*swap*/#&/g'  /etc/fstab
+
+# 修改内核参数
+modprobe br_netfilter
+echo "modprobe br_netfilter" >> /etc/profile
+cat > /etc/sysctl.d/k8s.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sysctl -p /etc/sysctl.d/k8s.conf
+
+# 配置阿里镜像源
+rm -rf /etc/yum.repos.d/*
+#base源：
+wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+#epel源：
+wget -O /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
+#docker源：
+yum install -y yum-utils device-mapper-persistent-data lvm2 vim wget bash-completion net-tools yum-utils tree 
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+#kubernetes源：
+cat > /etc/yum.repos.d/kubernetes.repo << EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=0
+EOF
+yum repolist
+
+# 配置时间同步
+yum install -y  ntpdate
+ntpdate cn.pool.ntp.org
+echo "* */1 * * * /usr/sbin/ntpdate   cn.pool.ntp.org" >> /var/spool/cron/root
+systemctl restart crond 
+
+# 开启ipvs
+if [ !-f "ipvs.modules" ]
+then
+echo "Missing files in the current directory: ipvs.modules"
+else
+mv ipvs.modules  /etc/sysconfig/modules/
+chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
+fi
+
+# 安装docker
+yum install -y docker-ce
+systemctl enabled docker --now
+
+# 配置docker镜像加速器
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+"registry-mirrors": ["https://t37y9kai.mirror.aliyuncs.com"],
+"exec-opts": ["native.cgroupdriver=systemd"]				
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+docker --version
+
+
+
+
+
--- shell/init.sh
+++ shell/init.sh
@@ -1,75 +1,73 @@
-# /bin/bash
+#!/bin/bash
 
-# 关闭防火墙和selinux
-systemctl disable firewalld --now
-setenforce 0		# 临时关闭
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config  # 永久关闭
+function isgreen() {
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32m$1==========>succeed\e[0m"
+    else
+        echo -e "\e[31m$1==========>failed\e[0m"
+    fi
+}
 
-# 关闭交换分区
-swapoff -a 		# 临时关闭
-sed -i 's/^[^#].*swap*/#&/g'  /etc/fstab
+function network() {
+    #超时时间
+    local timeout=1
 
-# 修改内核参数
-modprobe br_netfilter
-echo "modprobe br_netfilter" >> /etc/profile
-cat > /etc/sysctl.d/k8s.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-sysctl -p /etc/sysctl.d/k8s.conf
+    #目标网站
+    local target=www.baidu.com
 
-# 配置阿里镜像源
-rm -rf /etc/yum.repos.d/*
-#base源：
-wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
-#epel源：
-wget -O /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
-#docker源：
-yum install -y yum-utils device-mapper-persistent-data lvm2 vim wget bash-completion net-tools yum-utils tree 
-yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-#kubernetes源：
-cat > /etc/yum.repos.d/kubernetes.repo << EOF
-[kubernetes]
-name=Kubernetes
-baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
-enabled=1
-gpgcheck=0
-EOF
-yum repolist
+    #获取响应状态码
+    local ret_code=$(curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1)
 
-# 配置时间同步
-yum install -y  ntpdate
-ntpdate cn.pool.ntp.org
-echo "* */1 * * * /usr/sbin/ntpdate   cn.pool.ntp.org" >> /var/spool/cron/root
-systemctl restart crond 
+    if [ "$ret_code" = "200" ]; then
+        #网络畅通
+        echo -e "\e[32m网络畅通==========>succeed\e[0m"
+        return 0
+    else
+        #网络不畅通
+        echo -e "\e[31m网络不畅通，请检查网络==========>succeed\e[0m"
+        return 1
+    fi
 
-# 开启ipvs
-if [ !-f "ipvs.modules" ]
-then
-echo "Missing files in the current directory: ipvs.modules"
-else
-mv ipvs.modules  /etc/sysconfig/modules/
-chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
+    return 0
+}
+
+#关闭防火墙
+echo "关闭防火墙"
+systemctl disable firewalld --now >/dev/null 2>&1
+isgreen "关闭防火墙"
+
+#关闭selinux
+echo "关闭selinux"
+setenforce 0
+isgreen "临时关闭"
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+isgreen "永久关闭"
+
+#配置阿里源
+echo "开始配置阿里源"
+network
+code=$?
+if [ $code -eq 0 ]; then
+    rm -rf /etc/yum.repos.d/*
+    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo >/dev/null 2>&1
+    isgreen "base源"
+    curl -o /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo >/dev/null 2>&1
+    isgreen "epel源"
 fi
 
-# 安装docker
-yum install -y docker-ce
-systemctl enabled docker --now
-
-# 配置docker镜像加速器
-sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json <<-'EOF'
-{
-"registry-mirrors": ["https://t37y9kai.mirror.aliyuncs.com"],
-"exec-opts": ["native.cgroupdriver=systemd"]				
-}
-EOF
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-docker --version
+#安装常用软件
+echo "install vim wget bash-completion net-tools"
+yum install -y vim wget bash-completion net-tools >/dev/null 2>&1
+isgreen "install pkgs"
 
 
 
+#优化ssh连接
+echo "优化ssh连接"
+sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
+systemctl restart sshd
+isgreen 优化ssh连接
 
 
+echo -e "\e[32minitial option complete==========>succeed\e[0m"
