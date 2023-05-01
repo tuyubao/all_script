66c4e2e0cbc1b7ea6367f6835c96295b6376fa0a
tyb <19840981323@163.com>
2023-05-01 16:23:32 +0800

    manage folder

--- shell/optimize.sh
+++
@@ -1,9 +0,0 @@
-# ssh连接优化
-sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
-systemctl restart sshd
-
-# 安全优化
-systemctl disable firewalld --now
-setenforce 0		
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config  
